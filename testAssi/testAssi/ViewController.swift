c98884fca7861b5539508ca24f99c2e5013b79a2
Little-tale <you3192001@gmail.com>
2024-01-04 12:29:25 +0900

    StartTest

---
+++ testAssi/testAssi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testAssi
+//
+//  Created by Jae hyung Kim on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("Test")
+    }
+
+
+}
+
